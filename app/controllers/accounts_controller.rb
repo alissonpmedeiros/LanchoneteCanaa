c0d725d1b087071d26f40081031c65c5b45942bb
alisson <alissonp.medeiros@gmail.com>
2015-09-24 15:13:14 -0300

    configurando os customers nas views account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,7 @@
 class AccountsController < ApplicationController
   before_action :set_account, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :set_customers, only: [:new, :show, :edit]
   # GET /accounts
   # GET /accounts.json
   def index
@@ -65,6 +66,10 @@ class AccountsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_account
       @account = Account.find(params[:id])
+    end
+
+    def set_customers
+      @customers = Customer.all
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
